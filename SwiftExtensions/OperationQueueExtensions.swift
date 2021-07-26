303f68d4e769f8b39a823ebd6d78ceb0bb1c4f4b
Logan Cautrell <logan.cautrell@sentera.com>
2021-07-26 18:06:28 -0500

    Add extension to create new dispatch queue for serial operation queues.

--- SwiftExtensions/OperationQueueExtensions.swift
+++ SwiftExtensions/OperationQueueExtensions.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+public struct OperationQueueContext {
+    public let queue: OperationQueue
+    public let dispatchQueue: DispatchQueue
+}
+
 public extension OperationQueue {
     static let senteraUtilityQueue = DispatchQueue(label: "senteraUtilityQueue", qos: .utility)
     static func serialQueue(with name: String, qos: DispatchQoS.QoSClass = .default) -> OperationQueue {
@@ -22,5 +27,17 @@ public extension OperationQueue {
         }
         queue.name = "\(name)_OperationQueue"
         return queue
+    }
+
+    static func serialQueueContext(with name: String, qos: DispatchQoS = .default) -> OperationQueueContext {
+        let queue = OperationQueue()
+        queue.name = "\(name)_OperationQueue"
+        queue.maxConcurrentOperationCount = 1
+        queue.qualityOfService = .default
+
+        let dispatchQueue = DispatchQueue(label: name, qos: qos)
+        queue.underlyingQueue = dispatchQueue
+
+        return OperationQueueContext(queue: queue, dispatchQueue: dispatchQueue)
     }
 }
