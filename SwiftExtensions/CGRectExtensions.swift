785c5d4d673f3c4b61fa3de9a8f337ac71823949
Logan Cautrell <logan.cautrell@sentera.com>
2022-01-06 18:41:04 -0600

    Add new geometry functions.
    - Add new function for creating a rect from points.
    - Add new MKMapRect function without defaults.

--- SwiftExtensions/CGRectExtensions.swift
+++ SwiftExtensions/CGRectExtensions.swift
@@ -7,6 +7,19 @@
 
 import CoreGraphics
 
+// MARK: - Static
+
+public extension CGRect {
+    static func rectFrom(p1: CGPoint, p2: CGPoint) -> CGRect {
+        CGRect(x: min(p1.x, p2.x),
+               y: min(p1.y, p2.y),
+               width: abs(p1.x - p2.x),
+               height: abs(p1.y - p2.y)).integral
+    }
+}
+
+// MARK: - Instance
+
 public extension CGRect {
     func distanceFromCenter(to point: CGPoint) -> CGFloat {
         let xDist = midX - point.x
